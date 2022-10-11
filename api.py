775ea9d4e2d1e6b68f1150876d1d22ed6f7f93c9
TamerOliveira <30447351+TamerOliveira@users.noreply.github.com>
2022-10-11 13:05:18 -0300

    deploy

---
+++ api.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__) 
+		
+database = {
+    'wps@wps.com': ['1234', '12345678911'],
+    'caio@wps.com': ['4321', '12345678912'],
+    'tamer@wps.com': ['13579', '12345678913'],
+    'teste@wps.com': ['4231', '12345678914'],
+}
+
+@app.route('/')
+def index():
+    return 'ok', 200
+
+
+@app.route('/login', methods=['POST']) 
+def logins():
+    login = request.get_json()
+    user = login['username']
+    password = login['password']
+    if user not in database:
+        return 'login invalido', 401
+    else:
+        if password != database[user][0]:
+            return 'Senha invalida', 401
+        else:
+            return str(database[user][1]), 200
+
+if __name__ == '__main__':
+        app.run()
